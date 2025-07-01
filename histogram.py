33794e6cf5361d4d9d2a88059aed86083d197f63
Bagus Ario Yudanto <bagusarioyudanto123@gmail.com>
2025-07-01 18:15:53 +0700

    fix layout, add label in histogram, and fix requirements.txt

--- histogram.py
+++ histogram.py
@@ -53,50 +53,31 @@ if uploaded_file is not None:
             histograms[color] = hist
 
     column1, column2 = st.columns([0.5, 0.5])
-    column3, column4 = st.columns([0.5, 0.5])
+    _, column4 = st.columns([0.5, 0.5])
+    column5, column6 = st.columns([0.5, 0.5])
+    _, column8 = st.columns([0.5, 0.5])
 
     with st.container(border=True):
         with column1:
             st.image(grayscale_image, width=500, use_container_width=False)
 
         with column2:
-            st.bar_chart(histogram, color='#808080', height=500)
+            st.bar_chart(histogram,  x_label='Rentang Warna Grayscale', y_label='Frekuensi',color='#808080', height=500)
 
     st.write('')
 
     with st.container(border=True):
-        with column3:
-            st.image(rgb_image, width=500, channels='BGR', use_container_width=False)
-
         with column4:
             st.markdown("<h5 style='text-align: center;'>Merah</h5>", unsafe_allow_html=True)
-            st.bar_chart(histograms['Merah'], color='#FF0000', height=500)
-            st.write('Hijau')
-            st.bar_chart(histograms['Hijau'], color='#00FF00', height=500)
-            st.write('Biru')
-            st.bar_chart(histograms['Biru'], color='#0000FF', height=500)
-
-
-
-    # with column1:
-    #     container1.image(grayscale_image, width=300, use_container_width=False)
-    #     st.write('')
-    #     st.image(rgb_image, width=500, channels='BGR', use_container_width=False)
-
-    # with column2:
-    #     container1.bar_chart(histogram)
-
-    #     channels = {
-    #         'Merah': rgb_image[:, :, 0],
-    #         'Hijau': rgb_image[:, :, 1],
-    #         'Biru': rgb_image[:, :, 2]
-    #     }
-
-    #     histograms = {}
-    #     for color, channel in channels.items():
-    #         hist = cv2.calcHist([channel], [0], None, [256], [0, 256])
-    #         histograms[color] = hist / total_pixels
+            st.bar_chart(histograms['Merah'], x_label='Rentang Warna Merah', y_label='Frekuensi', color='#FF0000', height=500)
         
-    #     st.bar_chart(histograms['Merah'], height=500)
-    #     st.bar_chart(histograms['Hijau'], height=500)
-    #     st.bar_chart(histograms['Biru'], height=500)
+        with column5:
+            st.image(rgb_image, width=500, channels='BGR', use_container_width=False)
+
+        with column6:
+            st.markdown("<h5 style='text-align: center;'>Hijau</h5>", unsafe_allow_html=True)
+            st.bar_chart(histograms['Hijau'], x_label='Rentang Warna Hijau', y_label='Frekuensi', color='#00FF00', height=500)
+
+        with column8:
+            st.markdown("<h5 style='text-align: center;'>Biru</h5>", unsafe_allow_html=True)
+            st.bar_chart(histograms['Biru'], x_label='Rentang Warna Biru', y_label='Frekuensi', color='#0000FF', height=500)
